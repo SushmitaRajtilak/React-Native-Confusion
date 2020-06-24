9f7ac5c737348c2f2cb892280fab8a7b51d5f1aa
Sushmita Rajtilak <sushmita7898@gmail.com>
2020-06-24 16:06:02 +0530

    Activities and Favourites

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -6,13 +6,19 @@ import { COMMENTS } from '../shared/comments';
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
       dishes: state.dishes,
-      comments: state.comments
+      comments: state.comments,
+      favorites: state.favorites
     }
   }
+
+const mapDispatchToProps = dispatch => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId))
+})
   
 function RenderDish(props) {
     const dish = props.dish;
@@ -71,19 +77,11 @@ function RenderComments(props) {
 
 class DishDetail extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            comments: COMMENTS,
-            favorites: []
-        };
-        
-    }
 
     markFavorite(dishId) {
-        this.setState({favorites: this.state.favorites.concat(dishId)});
+        this.props.postFavorite(dishId);
     }
+
     
 
     static navigationOptions = {
@@ -95,7 +93,7 @@ class DishDetail extends Component {
         return(
             <ScrollView>
                 <RenderDish dish={this.props.dishes.dishes[+dishId]}
-                    favorite={this.state.favorites.some(el => el === dishId)}
+                     favorite={this.props.favorites.some(el => el === dishId)}
                     onPress={() => this.markFavorite(dishId)} 
                     />
                 <RenderComments comments={this.props.comments.comments.filter((comment) => comment.dishId === dishId)} />
@@ -104,4 +102,4 @@ class DishDetail extends Component {
     }
 }
 
-export default connect(mapStateToProps)(DishDetail);;
+export default connect(mapStateToProps, mapDispatchToProps)(DishDetail);
